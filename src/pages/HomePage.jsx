f535c1a478c16279cee9a48a9e392283793d4eb0
kwonbk82 <zkbk4982@gmail.com>
2025-09-09 18:15:06 +0900

    dldld

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,13 @@ const HomePage=()=>{
     return(
         <div id="HomePage">
             메인 페이지입니다!
-            <p>testt</p>
+            <p>slslslsl</p>
+            <p>slslslsl</p>
+            <p>slslslsl</p>
+            <p>slslslsl</p>
+            <p>slslslsl</p>
+            <p>slslslsl</p>
+            <p>slslslsl</p>
         </div>
     )
 }
