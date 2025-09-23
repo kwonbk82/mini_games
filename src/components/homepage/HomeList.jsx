d43d21b66d9701ac46c36b872db5de4a7675a556
kwonbk82 <zkbk4982@gmail.com>
2025-09-23 17:07:59 +0900

    rulepage css

--- src/components/homepage/HomeList.jsx
+++ src/components/homepage/HomeList.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
-
+import './HomeList.css';
 const HomeList = ({ lists }) => {
     
     return (
         <div id="HomeList">
+            <h1 className="list-name">{lists.name}</h1>
             <Link to={`/${lists.id}`}>
                 <img src={lists.img} alt={lists.name} />
             </Link>
-            <p className="list-name">{lists.name}</p>
         </div>
     );
 };
